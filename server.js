7e0e5e635630ebd54d78e1efc9b79817407c28cd
ayush <ayushsingh1372002@gmail.com>
2025-09-25 03:01:14 +0530

    rr

--- server.js
+++ server.js
@@ -25,10 +25,10 @@ app.use("/api/budget", budgetRoutes);
 app.get("/", (req, res) => res.send({ success: true, message: "Expense Tracker API" }));
 
 // connect to DB & start
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    app.listen(PORT, () => console.log(` Backend running on http://localhost:${PORT}`));
-  })
-  .catch((err) => {
-    console.error("DB connection error:", err);
-  });
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//     app.listen(PORT, () => console.log(` Backend running on http://localhost:${PORT}`));
+//   })
+//   .catch((err) => {
+//     console.error("DB connection error:", err);
+//   });
